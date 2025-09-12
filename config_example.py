a58687e0e2301f96f94feecd2140c40579f45ae0
Hagen <78751562+H4K0N42@users.noreply.github.com>
2025-09-12 18:52:45 +0200

    Hometime -> Bedtime

--- config_example.py
+++ config_example.py
@@ -1,32 +1,33 @@
 # Calendar
 
-CALENDAR = "INSERT YOUR CALENDAR NAME"          # "@group.calendar.google.com" also has to be on the end of the string (see 'get shareable link' on calendar on google 
-APIKEY = "INSERT YOUR APIKEY" 
-TIMEZONE = "Europe/Zurich"
-GOOGLECALBOOL = True                            # Boolean for whether to check google calendar page. If you do not want to use Google Calendar, set to False
-REFRESH = 15                                    # Seconds between updates of neopixel
-GOOGLE_REFRESH = 40                             # Cycles of updates before Google Calendar gets refreshed
-IGNORE_HARDCODED = False                        # Set to True if you want Clock in at the start of first meeting and Clockout at end of last meeting
-SCHEDULE = {                                    # This doesn't get used if IGNORE_HARDCODED is True. Othewise, it's the working hours for the week. (9.5 = 09:30 AM; 13 = 01:00 PM)
-    "monday":     [{"clockin": "8",   "clockout": "17"}],
-    "tuesday":    [{"clockin": "8",   "clockout": "17"}],
-    "wednesday":  [{"clockin": "8",   "clockout": "17"}],
-    "thursday":   [{"clockin": "8",   "clockout": "17"}],
-    "friday":     [{"clockin": "8",   "clockout": "17"}],
-    "saturday":   [{"clockin": "0",   "clockout": "0"}],    # I don't roll on shabbos
-    "sunday":     [{"clockin": "0",   "clockout": "0"}]
+TIMEZONE = "Europe/Berlin"
+SCHEDULE = {                                    # Input your bedtime (9.5 = 09:30 AM; 13 = 01:00 PM; 0 = OFF)
+    "monday":     [{"clockout": "22.5"}],
+    "tuesday":    [{"clockout": "22.5"}],
+    "wednesday":  [{"clockout": "22.5"}],
+    "thursday":   [{"clockout": "22.5"}],
+    "friday":     [{"clockout": "0"}],
+    "saturday":   [{"clockout": "0"}],
+    "sunday":     [{"clockout": "22.5"}]
 }
 
 # Neopixel
 
 PIXELS = 144                                    # The number of pixels on the neopixel strip
 GPIOPIN = 15                                    # The pin that the signal wire of the LED strip is connected to
-BARCOL = [(0, 100, 0), (0, 0, 100)]             # Color in RGB from 0 to 255
-EVENTCOL = [(255, 255, 255), (255, 255, 0)]     # list of tuples used as meeting colors, if you only use one: [(255, 255, 255)]
-DISPLAY_EVENTS = True                           # Shows events
 FLIP = False                                    # Set to True if you want to flip the bar
+
+REFRESH = 30                                    # Seconds between time updates and ping
+COLORS = [                                      # Pick any number of colors you like
+    "#0000ff",
+    "#00a0ff",
+    "#a000ff",
+    "#00a0ff",
+    "#0040ff"
+]
+DIM = 0.5                                       # Brightness of the Pixels 0..1; 1 = 100%
 
 # Other
 
-TWOCOL = False                                  # Displays a second bar that shows the progress of an event. While event is active, other events are not displayed anymore.
-DELWIFI = True                                  # Deletes wifi credentials if the connection fails
+DELWIFI = False                                 # Deletes wifi credentials if the connection fails
+PING = "http://192.168.178.42:80"               # Online when an HTTP server responds with status 200
